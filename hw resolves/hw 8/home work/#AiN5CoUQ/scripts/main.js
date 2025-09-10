35d6f7c7f10768a9c7d096742bc98b19da76b80e
RomanGit777 <rsavenko91@gmail.com>
2025-09-10 10:48:32 +0200

    working with hw 7

---
+++ hw resolves/hw 8/home work/#AiN5CoUQ/scripts/main.js
@@ -0,0 +1,25 @@
+// #AiN5CoUQ
+// Створити функцію, яка робить глибоку копію об’єкта.
+//     Додати перевірки на undefined, null, NaN.
+//     Подумати і реалізувати логіку, в якій кінцевий об’єкт буде мати функції, які в нього були до цього моменту.
+
+function cloner(obj) {
+    if (obj) {
+        const functions = [];
+        for (const key in obj) {
+            if (typeof obj[key] === 'function') {
+                const functionClone = obj[key].bind({});
+                functions.push({functionClone, key});
+            }
+        }
+        const cloneObj = JSON.parse(JSON.stringify(obj));
+        for (const func of functions) {
+            cloneObj[func.key] = func.functionClone;
+        }
+        console.log(cloneObj);
+        return cloneObj;
+    }
+    throw new Error('!!!');
+}
+let originalObj = {id: 123, name: 'qwe', greeting() {}, foo() {}}
+cloner(originalObj);
