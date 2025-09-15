51cafc2b1c63b18bba891f44ac12922b123337a8
RomanGit777 <rsavenko91@gmail.com>
2025-09-15 09:14:54 +0200

    done with hw 11

--- hw resolves/hw 11/home work/#whXxOBlYS0H/scripts/main.ts
+++ hw resolves/hw 11/home work/#whXxOBlYS0H/scripts/main.ts
@@ -1,40 +1,64 @@
 // #whXxOBlYS0H
 // – взяти https://dummyjson.com/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути списком під час відображення.
 
-const recipesDiv = document.getElementById('recipes');
+type RecipeType = {
+        id: number;
+        name: string;
+        ingredients: string[];
+        instructions: string[];
+        prepTimeMinutes: number;
+        cookTimeMinutes: number;
+        servings: number;
+        difficulty: string;
+        cuisine: string;
+        caloriesPerServing: number;
+        tags: string[];
+        userId: number;
+        image: string;
+        rating: number;
+        reviewCount: number;
+        mealType: string[];
+    }
+type IRecipeResponse = {
+    recipes: RecipeType[];
+    total: number;
+    skip: number;
+    limit: number;
+}
+const recipesDiv: HTMLDivElement = document.getElementById('recipes') as HTMLDivElement;
 
 fetch('https://dummyjson.com/recipes')
     .then(value => value.json())
-    .then(recipesObject => {
+    .then((recipesObject: IRecipeResponse) => {
         const {recipes} = recipesObject;
         console.log(recipes);
 
         for (const recipe of recipes) {
-            const div = document.createElement('div');
+            const div: HTMLDivElement = document.createElement('div');
             div.classList.add('div-container');
-            const divWithInfo = document.createElement('div');
+            const divWithInfo: HTMLDivElement = document.createElement('div');
             divWithInfo.innerText = `
                     id: ${recipe.id},
                     name: ${recipe.name}
                                     `
-            const img = document.createElement('img');
+            const img: HTMLImageElement = document.createElement('img');
             img.classList.add('img');
             img.src = `${recipe.image}`;
 
-                const ol = document.createElement('ol');
+                const ol: HTMLOListElement = document.createElement('ol');
             for (const ingredient of recipe.ingredients) {
-                const li = document.createElement('li');
+                const li: HTMLLIElement = document.createElement('li');
                 li.innerText = ingredient;
                     ol.append(li);
                 }
 
-            const time = document.createElement('p');
+            const time: HTMLParagraphElement = document.createElement('p');
             time.innerText = ` Time:
             prepTimeMinutes: ${recipe.prepTimeMinutes},
             cookTimeMinutes: ${recipe.cookTimeMinutes}
             `
 
-            const info = document.createElement('p');
+            const info: HTMLParagraphElement = document.createElement('p');
             info.innerText = `
              difficulty: ${recipe.difficulty},
              cuisine: ${recipe.cuisine},
