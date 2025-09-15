c2e707a74fcd930f956969474814db9279032b94
RomanGit777 <rsavenko91@gmail.com>
2025-09-15 19:50:03 +0200

    working with lesson 3

--- lesson 3/scripts/main.ts
+++ lesson 3/scripts/main.ts
@@ -18,29 +18,72 @@
 //
 
 
-interface IUserLS {
-    id: number;
-    name: string;
-}
+// interface IUserLS {
+//     id: number;
+//     name: string;
+// }
+//
+// let item = localStorage.getItem('user');
+//
+// if (item) {
+//     let user: IUserLS = JSON.parse(item) as IUserLS;
+// }
+//
+// interface IDog {
+//     aka: string;
+// }
+//
+// function foobar<T>(key: string): T {
+//     let objJSON = localStorage.getItem(key);
+//     if (objJSON) {
+//         return JSON.parse(objJSON) as T;
+//     }
+//     return {} as T;
+// }
+//
+// let iDog = foobar<IDog>('dog');
+//
+// let iUserLS = foobar<IUserLS>('user');
 
-let item = localStorage.getItem('user');
+// function reverseArray<T>(array: T[]): T[]{
+//     return [...array].reverse();
+// }
+//
+// type UsersType = { name: string, age: number, status: boolean };
+//
+// let users: UsersType[]  = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// console.log(reverseArray(users));
 
-if (item) {
-    let user: IUserLS = JSON.parse(item) as IUserLS;
-}
 
-interface IDog {
-    aka: string;
-}
+// function reverseArray<T>(arr: T[]): T[] {
+//     return [...arr].reverse();
+// }
 
-function foobar<T>(key: string): T {
-    let objJSON = localStorage.getItem(key);
-    if (objJSON) {
-        return JSON.parse(objJSON) as T;
-    }
-    return {} as T;
-}
-
-let iDog = foobar<IDog>('dog');
-
-let iUserLS = foobar<IUserLS>('user');
+// class Stack<T> {
+//     private arrayToStoreItems: T[] = [];
+//
+//     constructor(initialItems: T[] = []) {
+//         this.arrayToStoreItems = initialItems;
+//     }
+//     push(item: T): void{
+//         this.arrayToStoreItems.push(item);
+//     }
+//     pop(): T | undefined{
+//         return this.arrayToStoreItems.pop();
+//     }
+//     peek(): T | undefined{
+//        return this.arrayToStoreItems[this.arrayToStoreItems.length - 1];
+//     }
+// }
