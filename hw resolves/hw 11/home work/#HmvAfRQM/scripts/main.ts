51a88d776479a81c3618bbbdd95f833c379500f5
RomanGit777 <rsavenko91@gmail.com>
2025-09-15 08:34:34 +0200

    working with hw 11

--- hw resolves/hw 11/home work/#HmvAfRQM/scripts/main.ts
+++ hw resolves/hw 11/home work/#HmvAfRQM/scripts/main.ts
@@ -1,10 +1,35 @@
 // #HmvAfRQM
 // – взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+interface IProduct{
+    id: number;
+    title: string;
+    price: number;
+    quantity: number;
+    total: number;
+    discountPercentage: number;
+    discountedTotal: number;
+    thumbnail: string;
+}
+interface ICart {
+    id: number;
+    products: IProduct[];
+    total: number;
+    discountedTotal: number;
+    userId: number;
+    totalProducts: number;
+    totalQuantity: number;
+}
+interface ICartResponse {
+    carts: ICart[];
+    total: number;
+    skip: number;
+    limit: number;
+}
 const cartsDiv: HTMLDivElement = document.getElementById('carts') as HTMLDivElement;
 
 fetch('https://dummyjson.com/carts')
     .then(value => value.json())
-    .then(cartsObject => {
+    .then((cartsObject: ICartResponse) => {
         const {carts} = cartsObject;
         console.log(carts);
 
